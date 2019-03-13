17e5dc6f435096fb2237131cbd6ecffcdac7ef94
maykealisson <maykealison@gmail.com>
2019-03-13 20:39:08 -0300

    desenvolvendo sobre.ejs

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,11 +1,19 @@
 (function(win, doc){
   'use strict';
 
-  var $acordeao = doc.querySelector('.acordeao');
-
-  $acordeao.addEventListener('click', function(event){
-    console.log('Fui clicado');
-  }, false);
+  // Acordeão Padrão;
+  jQuery( '.acordeaoPadrao' ).each( function(){
+    jQuery(this).find( '.acordeao' ).click( function(){
+        if( jQuery(this).hasClass('ativo') ){
+            jQuery(this).removeClass('ativo');
+            jQuery(this).find('p').slideUp();
+        } else {
+            jQuery( '.acordeaoPadrao .acordeao' ).removeClass('ativo').find('p').slideUp();
+            jQuery(this).addClass('ativo');
+            jQuery(this).find('p').slideDown();
+        }
+    })
+  });
 
   // var $visor = doc.querySelector('[data-js="visor"]');
 
