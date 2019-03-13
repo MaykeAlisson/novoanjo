beeb2656952c2634ffa4eb683fdea6761eecf120
maykealisson <maykealison@gmail.com>
2019-03-13 12:31:12 -0300

    criando pagina sobre

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,6 +1,12 @@
 (function(win, doc){
   'use strict';
 
-    var $visor = doc.querySelector('[data-js="visor"]');
+  var $acordeao = doc.querySelector('.acordeao');
+
+  $acordeao.addEventListener('click', function(event){
+    console.log('Fui clicado');
+  }, false);
+
+  // var $visor = doc.querySelector('[data-js="visor"]');
 
 })(window, document);
