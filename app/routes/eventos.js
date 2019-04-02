749e0ca6d5f4b7a3753a38a4521bc89ec696f039
maykealisson <maykealison@gmail.com>
2019-04-02 05:27:16 -0300

    finalizing development view novoanjo.ejs

--- app/routes/eventos.js
+++ app/routes/eventos.js
@@ -7,13 +7,13 @@ module.exports = function(app) {
 
   // recebe req url
   app.get('/novo-evento', function(req, res) {
-    // envoca o metodo eventos na classe eventos
+    // envoca o metodo novo evento na classe eventos
     app.app.controllers.eventos.novoEvento(app, req, res);
   });
 
   // recebe req url
   app.post('/novo-evento', function(req, res) {
-    // envoca o metodo eventos na classe eventos
+    // envoca o metodo novo evento na classe eventos
     app.app.controllers.eventos.novoEventoRegister(app, req, res);
   });
 };
