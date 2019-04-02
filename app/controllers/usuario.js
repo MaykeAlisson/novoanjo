af74203a4e7d31a6aabaf2ee9753fb650c1e9c42
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-01 23:30:03 -0300

    feat(user): add user profile dinamic rendering

---
+++ app/controllers/usuario.js
@@ -0,0 +1,23 @@
+var mockUsuarios = [
+  {
+    id: '1',
+    nome: 'Leonardo',
+    sobrenome: 'Xavier',
+    email: 'gxavier.leonardo@gmail.com',
+    telefone: '2422453789',
+    organizacao: null,
+    anjo: true,
+    beneficiario: false
+  }
+];
+
+module.exports = {
+  usuario(app, req, res, idUsuario) {
+    var usuario = mockUsuarios.filter(function({ id }) {
+      return id === idUsuario;
+    })[0];
+
+    // render da pagina
+    res.render('usuario', { usuario });
+  }
+};
