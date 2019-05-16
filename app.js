e9e3a2b30f9495b73e0f821ac23a9689d622fe44
Luiz G Miranda <luiz.miranda@portalunimed.com.br>
2019-05-16 10:33:54 -0300

    Melhoria na mensagem de servidor on

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //  Importando server.js
 var app = require('./config/server');
+var port = 4000
 
 // Config servidor
-app.listen(3000, function () {
-    console.log('Servidor On...');
+app.listen(port, function () {
+    console.log(`Servidor online na porta: ${ port }`);
 });
