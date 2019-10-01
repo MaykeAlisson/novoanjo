18641836766c839edf8dc3dd267d55421d5b90c0
Mayke Alisson <mayke.furtado@arcom.com.br>
2019-10-01 08:32:16 -0300

    Connect

---
+++ config/connectionFactory.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'XXX',
+  port     : XXX,
+  user     : 'XXX',
+  password : 'XXX',
+  database : 'XXX'
+});
