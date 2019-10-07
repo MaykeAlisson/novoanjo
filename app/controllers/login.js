2fadb4f193453523e439d16450171f6f8d7d5ca0
Mayke Alisson <mayke.furtado@arcom.com.br>
2019-10-07 10:48:06 -0300

    erros apos merge

--- app/controllers/login.js
+++
@@ -1,4 +0,0 @@
-module.exports.contato = function(app, req, res){
-    // render pagina
-    res.render('contato');
-};
--- app/routes/perfil.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-  // recebendo req url
-  app.get('/sobre', function (req, res) {
-    // Envoca metodo sobre na classe sobre
-    app.app.controllers.sobre.sobre(app, req, res);
-  });
-}
